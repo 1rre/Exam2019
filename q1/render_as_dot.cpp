3eec99d4702cbd829c58da0a96b8b8688473ba14
tjm1518 <tjm1518@ic.ac.uk>
2020-05-04 22:16:48 +0100

    added rendering of nodes as dot

--- q1/render_as_dot.cpp
+++ q1/render_as_dot.cpp
@@ -1,6 +1,7 @@
 #include "tree.hpp"
 #include "string_set.hpp"
 
+string renderNode(const tree_node* n);
 
 int main(int argc, char **argv)
 {
@@ -23,7 +24,25 @@ int main(int argc, char **argv)
 
     cout << "digraph G { "<<endl;
 
-    // TODO: render the links between nodes
+    cout << renderNode(r);
 
     cout << "}" << endl;
+}
+
+string renderNode(const tree_node* n){
+		string rtn = "\n";
+		if(n->left!=nullptr){
+				rtn += ("\""+n->value+"\" -> \""+n->left->value+"\" [label=\"L\"]\n");
+		}
+		if(n->right!=nullptr){
+				rtn += ("\""+n->value+"\" -> \""+n->right->value+"\" [label=\"R\"]\n");
+		}
+				rtn += "\n";
+		if(n->left!=nullptr){
+				rtn += renderNode(n->left);
+		}
+		if(n->right!=nullptr){
+				rtn += renderNode(n->left);
+		}
+		return rtn;
 }
