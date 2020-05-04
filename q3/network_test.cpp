75f42c26381acc6ff5b2a5e2585895ed32e35a1c
tjm1518 <tjm1518@ic.ac.uk>
2020-05-04 17:07:54 +0100

    tested T3.2

--- q3/network_test.cpp
+++ q3/network_test.cpp
@@ -1,7 +1,13 @@
 #include "network.hpp"
 
 int main(int argc, char* argv[]){
-		Network n;
-		cin>>n;
-		cout<<impedance(n,(float)0.5);
+		Network n,n0;
+		cin>>n>>n0;
+		vector<float> omega;
+		for(float i = 1; i<10;i++){
+				omega.push_back(i);
+		}
+		for(complex<float> x :transfer_function(n,n0,omega)){
+				cout<<x<<endl;
+		}
 }
