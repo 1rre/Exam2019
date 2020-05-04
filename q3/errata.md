ffae49aafc86d4c28bb2f31ca651fe02206a8d7e
tjm1518 <tjm1518@ic.ac.uk>
2020-05-04 16:59:29 +0100

    implemented transfer functions

--- q3/errata.md
+++ q3/errata.md
@@ -1,3 +1,15 @@
  print_component_list.cpp appears to already have been implemented. (T1.2)
- in readme.html it is stated that '&<C<L<R<|', however an example of ordering primitives it states 'C0.5 < C1 < C2 < R1e-3 < R1 < L0.3 < L10'. I have assumed that L<R. (T1.3)
+ in readme.html it is stated that '&<C<L<R<|', however an example of ordering primitives it states 'C0.5 < C1 < C2 < R1e-3 < R1 < L0.3 < L10'. I have assumed that L\<R. (T1.3)
  No clear indication of which of (R1|R2|R3|R4) and (R1|R2|R3) is greater. I have assumed that in the case of |, the former is greater, however if we were to replace | with &, the latter would be greater.
+ Although it was stated that n1 and n2 formed a potential divider, it didn't state where measurements were to be taken. I assumed that they were in the following configuration:
+```
+ X---n1---+---Y
+          |
+          |
+          n2
+          |
+          |
+				_____
+				 ___
+				  _
+```
--- q3/network_impedance.cpp
+++ q3/network_impedance.cpp
@@ -20,19 +20,22 @@ complex<float> impedance(const Network &c, float omega)
 		}
 		else{ //if c is a parallel network
 				complex<float> rtn = {0,0};
-				for(Network x : c.parts){
-						rtn += ((float)1)/(impedance(x,omega));
+				for(Network x : c.parts){ //for each subnetwork in c
+						rtn += ((float)1)/(impedance(x,omega)); //add (impedance of subnetwork)⁻¹ to the return value
 				}
-				return ((float)1)/rtn;
+				return ((float)1)/rtn; //return (the return value)⁻¹
 		}
 }
 
 vector<complex<float>> transfer_function(const Network &v1, const Network &v2, const vector<float> &omega)
 {
     vector<complex<float>> res;
-
-    // TODO
-
+		for(float freq : omega){ //for each frequency value
+				complex<float> n1 = impedance(v1, freq); //set v1's impedance at a given omega
+				complex<float> n2 = impedance(v2, freq); //set v2's impedance at a given omega
+				complex<float> tf = n2/(n1+n2); //the transfer function of n1 & n2 given the assumption in errata.md is made
+				res.push_back(tf); //add the value of the transfer function at omega to the corresponding place in res
+		}
     return res;
 }
 
